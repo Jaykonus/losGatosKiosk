55e081fbb0e870e71fd610a0e4c3bd051a2fc6ca
Jake Honas <j24honas@ksu.edu>
2020-04-09 12:19:56 -0500

    Add project files.

---
+++ losGatosKiosk/frmKiosk.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace losGatosKiosk
+{
+    partial class frmKiosk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.lblWelcome2 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblUnit = new System.Windows.Forms.Label();
+            this.grpDetails = new System.Windows.Forms.GroupBox();
+            this.lblInfo2 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboPriority = new System.Windows.Forms.ComboBox();
+            this.lblPriority = new System.Windows.Forms.Label();
+            this.lblEquipment = new System.Windows.Forms.Label();
+            this.cboUnit = new System.Windows.Forms.ComboBox();
+            this.cboEquipment = new System.Windows.Forms.ComboBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblEmployee = new System.Windows.Forms.Label();
+            this.txtEmployee = new System.Windows.Forms.TextBox();
+            this.grpDetails.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(104, 65);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(490, 28);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "Thank you for using Los Gatos ticket generator!";
+            // 
+            // lblWelcome2
+            // 
+            this.lblWelcome2.AutoSize = true;
+            this.lblWelcome2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome2.Location = new System.Drawing.Point(104, 115);
+            this.lblWelcome2.Name = "lblWelcome2";
+            this.lblWelcome2.Size = new System.Drawing.Size(534, 25);
+            this.lblWelcome2.TabIndex = 1;
+            this.lblWelcome2.Text = "Please fill out all elements before submitting weld ticket.";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(22, 337);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(130, 69);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblUnit
+            // 
+            this.lblUnit.AutoSize = true;
+            this.lblUnit.Location = new System.Drawing.Point(17, 41);
+            this.lblUnit.Name = "lblUnit";
+            this.lblUnit.Size = new System.Drawing.Size(55, 25);
+            this.lblUnit.TabIndex = 3;
+            this.lblUnit.Text = "Unit:";
+            // 
+            // grpDetails
+            // 
+            this.grpDetails.Controls.Add(this.txtEmployee);
+            this.grpDetails.Controls.Add(this.lblEmployee);
+            this.grpDetails.Controls.Add(this.btnReset);
+            this.grpDetails.Controls.Add(this.cboEquipment);
+            this.grpDetails.Controls.Add(this.lblInfo2);
+            this.grpDetails.Controls.Add(this.btnSubmit);
+            this.grpDetails.Controls.Add(this.btnExit);
+            this.grpDetails.Controls.Add(this.txtInfo);
+            this.grpDetails.Controls.Add(this.label1);
+            this.grpDetails.Controls.Add(this.cboPriority);
+            this.grpDetails.Controls.Add(this.lblPriority);
+            this.grpDetails.Controls.Add(this.lblEquipment);
+            this.grpDetails.Controls.Add(this.cboUnit);
+            this.grpDetails.Controls.Add(this.lblUnit);
+            this.grpDetails.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpDetails.Location = new System.Drawing.Point(30, 171);
+            this.grpDetails.Name = "grpDetails";
+            this.grpDetails.Size = new System.Drawing.Size(679, 423);
+            this.grpDetails.TabIndex = 4;
+            this.grpDetails.TabStop = false;
+            this.grpDetails.Text = "Ticket Details";
+            // 
+            // lblInfo2
+            // 
+            this.lblInfo2.AutoSize = true;
+            this.lblInfo2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo2.Location = new System.Drawing.Point(18, 260);
+            this.lblInfo2.Name = "lblInfo2";
+            this.lblInfo2.Size = new System.Drawing.Size(86, 21);
+            this.lblInfo2.TabIndex = 12;
+            this.lblInfo2.Text = "(Optional)";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Enabled = false;
+            this.btnSubmit.Location = new System.Drawing.Point(450, 27);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(211, 104);
+            this.btnSubmit.TabIndex = 11;
+            this.btnSubmit.Text = "Submit Ticket";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(176, 243);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(485, 163);
+            this.txtInfo.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Additional Info:";
+            // 
+            // cboPriority
+            // 
+            this.cboPriority.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPriority.Enabled = false;
+            this.cboPriority.FormattingEnabled = true;
+            this.cboPriority.Items.AddRange(new object[] {
+            "Low",
+            "Medium",
+            "High",
+            "URGENT"});
+            this.cboPriority.Location = new System.Drawing.Point(176, 147);
+            this.cboPriority.Name = "cboPriority";
+            this.cboPriority.Size = new System.Drawing.Size(256, 33);
+            this.cboPriority.TabIndex = 8;
+            this.cboPriority.SelectedIndexChanged += new System.EventHandler(this.cboPriority_SelectedIndexChanged);
+            // 
+            // lblPriority
+            // 
+            this.lblPriority.AutoSize = true;
+            this.lblPriority.Location = new System.Drawing.Point(17, 147);
+            this.lblPriority.Name = "lblPriority";
+            this.lblPriority.Size = new System.Drawing.Size(135, 25);
+            this.lblPriority.TabIndex = 7;
+            this.lblPriority.Text = "Priority Level:";
+            // 
+            // lblEquipment
+            // 
+            this.lblEquipment.AutoSize = true;
+            this.lblEquipment.Location = new System.Drawing.Point(17, 98);
+            this.lblEquipment.Name = "lblEquipment";
+            this.lblEquipment.Size = new System.Drawing.Size(142, 25);
+            this.lblEquipment.TabIndex = 5;
+            this.lblEquipment.Text = "Equipment ID:";
+            // 
+            // cboUnit
+            // 
+            this.cboUnit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboUnit.FormattingEnabled = true;
+            this.cboUnit.Items.AddRange(new object[] {
+            "Division 1",
+            "Division 2",
+            "Division 3",
+            "Maintenance"});
+            this.cboUnit.Location = new System.Drawing.Point(176, 38);
+            this.cboUnit.Name = "cboUnit";
+            this.cboUnit.Size = new System.Drawing.Size(256, 33);
+            this.cboUnit.TabIndex = 4;
+            this.cboUnit.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cboEquipment
+            // 
+            this.cboEquipment.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboEquipment.Enabled = false;
+            this.cboEquipment.FormattingEnabled = true;
+            this.cboEquipment.Location = new System.Drawing.Point(176, 95);
+            this.cboEquipment.Name = "cboEquipment";
+            this.cboEquipment.Size = new System.Drawing.Size(256, 33);
+            this.cboEquipment.TabIndex = 13;
+            this.cboEquipment.SelectedIndexChanged += new System.EventHandler(this.cboEquipment_SelectedIndexChanged);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(450, 147);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(211, 66);
+            this.btnReset.TabIndex = 14;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblEmployee
+            // 
+            this.lblEmployee.AutoSize = true;
+            this.lblEmployee.Location = new System.Drawing.Point(17, 198);
+            this.lblEmployee.Name = "lblEmployee";
+            this.lblEmployee.Size = new System.Drawing.Size(106, 25);
+            this.lblEmployee.TabIndex = 15;
+            this.lblEmployee.Text = "Employee:";
+            // 
+            // txtEmployee
+            // 
+            this.txtEmployee.Enabled = false;
+            this.txtEmployee.Location = new System.Drawing.Point(176, 195);
+            this.txtEmployee.Name = "txtEmployee";
+            this.txtEmployee.Size = new System.Drawing.Size(256, 33);
+            this.txtEmployee.TabIndex = 16;
+            this.txtEmployee.TextChanged += new System.EventHandler(this.txtEmployee_TextChanged);
+            // 
+            // frmKiosk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 606);
+            this.ControlBox = false;
+            this.Controls.Add(this.grpDetails);
+            this.Controls.Add(this.lblWelcome2);
+            this.Controls.Add(this.lblWelcome);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "frmKiosk";
+            this.Text = "Los Gatos Morados Ticket Kiosk";
+            this.grpDetails.ResumeLayout(false);
+            this.grpDetails.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Label lblWelcome2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblUnit;
+        private System.Windows.Forms.GroupBox grpDetails;
+        private System.Windows.Forms.ComboBox cboUnit;
+        private System.Windows.Forms.Label lblEquipment;
+        private System.Windows.Forms.ComboBox cboPriority;
+        private System.Windows.Forms.Label lblPriority;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInfo2;
+        private System.Windows.Forms.ComboBox cboEquipment;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtEmployee;
+        private System.Windows.Forms.Label lblEmployee;
+    }
+}
+
---
+++ losGatosKiosk/frmKiosk.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace losGatosKiosk
+{
+    public partial class frmKiosk : Form
+    {
+        public frmKiosk()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cboEquipment.Enabled = true;
+            cboEquipment.Items.Clear();
+            cboEquipment.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            if (cboUnit.SelectedItem == "Division 1")
+            {
+                cboEquipment.Items.Add(1);
+                cboEquipment.Items.Add(2);
+                cboEquipment.Items.Add(3);
+                cboEquipment.Items.Add(4);
+                cboEquipment.Items.Add(5);
+            }
+            else if (cboUnit.SelectedItem == "Division 2")
+            {
+                cboEquipment.Items.Add(6);
+                cboEquipment.Items.Add(7);
+                cboEquipment.Items.Add(8);
+                cboEquipment.Items.Add(9);
+                cboEquipment.Items.Add(10);
+            }
+            else if (cboUnit.SelectedItem == "Division 3")
+            {
+                cboEquipment.Items.Add(11);
+                cboEquipment.Items.Add(12);
+                cboEquipment.Items.Add(13);
+                cboEquipment.Items.Add(14);
+                cboEquipment.Items.Add(15);
+            }
+            else if (cboUnit.SelectedItem == "Maintenance")
+            {
+                cboEquipment.Items.Add(16);
+                cboEquipment.Items.Add(17);
+                cboEquipment.Items.Add(18);
+            }
+        }
+
+        private void cboEquipment_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cboPriority.Enabled = true;
+        }
+
+        private void cboPriority_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtEmployee.Enabled = true;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to submit ticket?", "Ticket Submission", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                //do something
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+            cboUnit.SelectedIndex = -1;
+            //cboEquipment.Items[cboEquipment.SelectedIndex] = "";
+            cboPriority.SelectedIndex = -1;
+            cboPriority.Enabled = false;
+            cboEquipment.Enabled = false;
+            txtInfo.Text = "";
+            txtEmployee.Text = "";
+           
+            
+        }
+
+        private void txtEmployee_TextChanged(object sender, EventArgs e)
+        {
+            if(txtEmployee.Text.Trim().Length > 0)
+            {
+                btnSubmit.Enabled = true;
+            }
+            else if(txtEmployee.Text.Trim().Length == 0)
+            {
+                btnSubmit.Enabled = false;
+            }
+        }
+    }
+}
